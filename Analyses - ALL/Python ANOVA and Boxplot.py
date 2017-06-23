82ea61be9fd595b51c27cb691ceba9969de78884
beakersandbarbells <jdbergren@gmail.com>
2017-06-23 16:14:58 -0500

    Generated new boxplots with ANOVA stat-test data!

--- Analyses - ALL/Python ANOVA and Boxplot.py
+++ Analyses - ALL/Python ANOVA and Boxplot.py
@@ -3,7 +3,7 @@ from scipy import stats
 import numpy as np
 from matplotlib import pyplot as plt
 
-df = pd.read_pickle('C:\Users\m144851\Desktop\Github Projects\Donald-vs-CRISPR\Data\Clean\AllCRISPRTweetsdataframe.pkl')
+df = pd.read_pickle('C:\Users\m144851\Desktop\Github Projects\Donald-vs-CRISPR\Data\Clean\AllMysteryTweetsdataframe.pkl')
 
 df = df.drop(df[np.isnan(df.Tweettextscore) == True].index)
 
@@ -19,34 +19,50 @@ moncleanscores = mondaytweets.Tweettextscore.dropna()
 tuescleanscores = tuesdaytweets.Tweettextscore.dropna()
 wedscleanscores = wednesdaytweets.Tweettextscore.dropna()
 
-#df.boxplot('Tweettextscore', by='tweetweekday')
 grps = pd.unique(df.tweetweekday.values)
 d_df = {grp:df['Tweettextscore'][df.tweetweekday == grp] for grp in grps}
 k = len(pd.unique(df.tweetweekday))
 N = len(df.Tweettextscore.values)
+Nvalues = df.groupby('tweetweekday').size().values
+
 n0 = df.groupby('tweetweekday').size()[0]
 n1 = df.groupby('tweetweekday').size()[1]
 n2 = df.groupby('tweetweekday').size()[2]
 n3 = df.groupby('tweetweekday').size()[3]
 n4 = df.groupby('tweetweekday').size()[4]
 
+NValueslist = [n0, n1, n2, n3, n4]
+
 f, p = stats.f_oneway(d_df['Saturday'], d_df['Sunday'], d_df['Monday'], d_df['Tuesday'], d_df['Wednesday'])
 
-plt.boxplot([satcleanscores.values, suncleanscores, moncleanscores, tuescleanscores, wedscleanscores], notch=True, labels=['Saturday' + '\n' + 'N: '+ str(n0), 'Sunday', 'Monday', 'Tuesday', 'Wednesday'])
-plt.axhline(y=0, lw=0.5, ls='dashed')
-plt.xlabel('Day of the Week')
-plt.ylabel('Sentiment Score')
-plt.title('CRISPR Sentiment Scores')
-plt.savefig('CRISPR Sentiment Score Boxplots.png')
+p = round(p, 6)
+f = round(f, 2)
 
-print 'F Value: ' + str(round(f, 2))
-print 'P Value: ' + str(p)
-print 'Unique groups: ' + str(k)
-print 'Number of Observations: ' + str(N)
-print 'N-Saturday: ' + str(n0)
-print 'N-Sunday: ' + str(n1)
-print 'N-Monday: ' + str(n2)
-print 'N-Tuesday: ' + str(n3)
-print 'N-Wednesday: ' + str(n4)
+statanswers = [f, p, k, N]
+statnames = ['F', 'P-value', 'Unique Groups', 'Total Tweets Analyzed']
+
+fig = plt.figure(1, figsize=(6, 10))
+plt.subplot(2,1,1)
+plt.boxplot([satcleanscores.values, suncleanscores, moncleanscores, tuescleanscores, wedscleanscores], notch=True)
+frame1 = plt.gca()
+frame1.axes.get_xaxis().set_visible(False)
+plt.axhline(y=0, lw=0.5, ls='dashed')
+plt.ylabel('Sentiment Score')
+plt.title('John Cena Sentiment Scores')
+
+table1 = plt.table(cellText=[NValueslist], colLabels=['Saturday', 'Sunday', 'Monday', 'Tuesday', 'Wednesday'], loc='bottom', cellLoc='center', rowLabels=['N'])
+table1.auto_set_font_size(False)
+table1.set_fontsize(10)
+
+plt.subplot(2,1,2)
+table2 = plt.table(label=['ANOVA'], cellText=[statanswers], loc='center', cellLoc='center', colLabels=['F', 'P-value', 'Unique Groups', 'Total N'])
+plt.axis('off')
+
+table2.auto_set_font_size(False)
+table2.set_fontsize(10)
+
+plt.tight_layout(h_pad=-10)
+
+plt.savefig('John Cena Sentiment Score Boxplots with ANOVA.png')
 
 plt.show()
